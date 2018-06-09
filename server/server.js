739fd0677c35980d7130f26cba0a767cd1315865
Jack Langston <jclangst@indiana.edu>
2018-06-09 17:15:45 -0400

    Cleaned up the server entrypoint

--- server/server.js
+++ server/server.js
@@ -10,43 +10,56 @@ import admin from "firebase-admin";
 import api from "./lib/routes/api.js";
 import serviceAccount from "./_firebase.js";
 
-const publicDir = path.resolve(__dirname, "../dist");
-console.log(publicDir);
-const app = express();
+//set up the public directory
+
+
+//establish the firebase connection TODO i'd probably move this to a separate folder
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
     databaseURL: "https://galactic-storage.firebaseio.com"
 });
 const firedb = admin.firestore();
 
-app.use(bodyParser.json());
-app.use("/gql", bodyParser.json(), graphqlExpress({ schema: schema }));
-app.use("/iql", graphiqlExpress({ endpointURL: "/gql" }));
-app.use(function(req, res, next) {
-    req.db = res.db = firedb;
-    res.header(
-        "Access-Control-Allow-Headers",
-        "Origin, X-Requested-With, Content-Type, Accept"
-    );
-    res.header(
-        "Access-Control-Allow-Methods",
-        "GET, POST, PUT, DELETE, OPTIONS"
-    );
-    res.append("Content-Type", "application/json");
-    next();
-});
-app.use("/api", api.router);
 
-app.get("/", function(req, res) {
-    console.log(path.resolve(publicDir, "./index.html"));
-    res.sendFile(path.resolve(publicDir, "./index.html"));
-});
+/**************************************
+ * Attach all of the Express Middleware
+ **************************************/
+
+const app = express();
+
 
 app.use(
     bodyParser.urlencoded({
         extended: true
     })
 );
+
+//graphql attachments
+app.use("/gql", bodyParser.json(), graphqlExpress({ schema: schema }));
+app.use("/iql", graphiqlExpress({ endpointURL: "/gql" }));
+
+
+//TODO figure out what this is -- we really should be using a cors middleware
+// //if we are allow cross domain access
+// app.use(function(req, res, next) {
+//     req.db = res.db = firedb;
+//     res.header(
+//         "Access-Control-Allow-Headers",
+//         "Origin, X-Requested-With, Content-Type, Accept"
+//     );
+//     res.header(
+//         "Access-Control-Allow-Methods",
+//         "GET, POST, PUT, DELETE, OPTIONS"
+//     );
+//
+//     //TODO this is clearly not correct as we are NOT returning JSON at this point
+//     res.append("Content-Type", "application/json");
+//     next();
+// });
+
+
+//set the static asset directory
+const publicDir = path.resolve(__dirname, "../dist");
 app.use(express.static(publicDir));
 app.use(fallback(path.resolve(publicDir, "./index.html")));
 
