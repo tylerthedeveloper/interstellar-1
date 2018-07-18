3bbfb647f4453a8082485667a4e02ff3cad909cd
Tyler Citrin <tcitrin@indiana.edu>
2018-07-18 00:06:03 -0700

    no trust issues

--- checkout-test/cart-items-data.js
+++ checkout-test/cart-items-data.js
@@ -10,7 +10,7 @@ const cartItems = [
         name: "CartItem 1",
         // price: 20,
         fixedUSDAmount: 20,
-        acceptedAsset: { asset_type: "native", asset_code: "XLM" },
+        acceptedAsset: { balance: 0, asset_type: "alpha-numeric-4", asset_code: "BTC" },
         description: "This is a product.",
         rating: 2
     },
--- checkout-test/testGetUsd.js
+++ checkout-test/testGetUsd.js
@@ -1,92 +1,79 @@
-// /** @flow */
-// // @flow
-//
-// const axios = require('axios');
-//
-// // can use this for parsing response and type safety, but not needed
-// // type Ticker {
-// //     id: string;
-// //     name;
-// //     symbol;
-// //     website_slug;
-// // }
-//
-// // todo: ensure assetSymbol is provided
-//
-// async function getTickerList () {
-//     return await axios.get('https://api.coinmarketcap.com/v2/listings/')
-//                         .then((response) => response.data.data);
+
+const axios = require('axios');
+
+// can use this for parsing response and type safety, but not needed
+// type Ticker {
+//     id: string;
+//     name;
+//     symbol;
+//     website_slug;
 // }
 //
-// getTickerID = (tickerList, assetSymbol) => {
-//     for (const ticker of tickerList) {
-//         if(ticker['symbol'] == assetSymbol) {
-//             return ticker['id'];
-//         }
-//     }
-//     return 0;
-// }
-//
-// // getTickerID = (assetSymbol) => {
-// //    return axios.get('https://api.coinmarketcap.com/v2/listings/')
-// //         .then((response) => response.data.data)
-// //         .then((data) => {
-// //             for (const ticker of data) {
-// //                 if(ticker['symbol'] == assetSymbol) {
-// //                     return ticker['id'];
-// //                 }
-// //             }
-// //             return 0;
-// //         }
-// //     );
-// // }
-//
-// // todo: ensure assetID is provided
-// async function getUsdPriceForTickerID (assetID) {
-//     return await axios.get(`https://api.coinmarketcap.com/v2/ticker/${assetID}`)
-//         .then((response) => response.data.data)
-//         .then((data) => data.quotes.USD.price)
-// }
-//
-// //
-// // just have temp comments for data validation
-// //
-// // getTickerID("ETH")
-// //     // if (tickerID)
-// //     //     console.log(tickerID);
-// //     // else ... need to enter a ticker symbol
-// //     .then(tickerID => getUsdPriceForTickerID(tickerID)
-// //         // const tickerPrice =  getUsdPriceForTickerID( 1 /** tickerID */);
-// //         // if (tickerPrice)
-// //         //     console.log(tickerPrice);
-// //         // else ... need to enter a ticker id number
-// //         .then(tickerPrice => console.log(tickerPrice))
-// //     )
-//
-// // const tickerList =  (async () => await getTickerList().then((response) => response.data.data)) // .then((response) => console.log(response.data.data));
-//
-// const assetSymbols = ["BTC", "ETH"];
-//
-// getTickerList()
-//     .then(listings => {
-//         const tickerList = assetSymbols.map(symbol => ({symbol: symbol, tickerID: getTickerID(listings, symbol)}));
-//         console.log(tickerList)
-//         const priceList = tickerList.map(tickerDict =>
-//         ({
-//             symbol: tickerDict.symbol,
-//             tickerID: tickerDict.tickerID,
-//
-//             // get these prices in a list then merge them in with a thenable
-//             price: await getUsdPriceForTickerID(tickerDict.tickerID)
-//
-//
-//         }));
-//         Promise.sellers(priceList).then(d => console.log(d));
-//     });
-//
-//
-// module.exports = {
-//     getTickerList,
-//     getTickerID,
-//     getUsdPriceForTickerID
-// }
+
+// todo: ensure assetSymbol is provided
+async function getTickerList () {
+    return await axios.get('https://api.coinmarketcap.com/v2/listings/')
+        .then((response) => response.data.data);
+}
+
+// rather than load the listings every time, only have to load it once
+// as done in the below version of the function 
+function getTickerID (tickerList, assetSymbol) {
+    for (const ticker of tickerList) {
+        if(ticker['symbol'] == assetSymbol) {
+            return ticker['id'];
+        }
+    }
+    return 0;
+}
+
+// loads entire list but shouldnt be needed
+function getNewTickerID (assetSymbol) {
+   return axios.get('https://api.coinmarketcap.com/v2/listings/')
+        .then((response) => response.data.data)
+        .then((data) => {
+            for (const ticker of data) {
+                if(ticker['symbol'] == assetSymbol) {
+                    return ticker['id'];
+                }
+            }
+            return 0;
+        }
+    );
+}
+
+// todo: ensure assetID is provided
+async function getUsdPriceForTickerID (assetID) {
+    return await axios.get(`https://api.coinmarketcap.com/v2/ticker/${assetID}`)
+        .then((response) => response.data.data)
+        .then((data) => data.quotes.USD.price)
+} 
+
+function runner (assetSymbolList) {
+    return getTickerList()
+        .then(listings => {
+            const tickerList = assetSymbolList.map(symbol => ({symbol: symbol, tickerID: getTickerID(listings, symbol)}));
+            const promiseList = tickerList.map(tickerDict => getUsdPriceForTickerID(tickerDict.tickerID));
+            return Promise.all(promiseList).then(priceList => 
+            {
+                tickerList.forEach(function (ticker, index) {
+                    ticker['price'] = priceList[index];
+                });
+                return tickerList;
+            });
+        }
+    );
+}
+
+//      //
+// test //
+//      //
+const assetSymbols = ["BTC", "ETH"];
+// runner(assetSymbols).then(res => console.log(res));
+
+module.exports = {
+    getTickerList,
+    getTickerID,
+    getUsdPriceForTickerID,
+    runner
+}
